56b14d2e2e6218f88ff805862006a127dcedb03f
George Cavazos <cavazosgeorge@yahoo.com>
2021-07-15 20:58:05 -0400

    Complete Home Landing Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 export default function home() {
   return (
-    <div>
-      <Link to="/nasaphoto">See Into The Stars!</Link>
-    </div>
+    <>
+      <NavBar />
+      <div className="home">
+        <Link className="home-link" to="/nasaphoto">
+          See Into The Stars!
+        </Link>
+      </div>
+    </>
   );
 }
